8f8ec5ffcbca25cea2ce2d9e9b6e2617474791de
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-09-14 10:17:50 -0700

    Added basic draft functionality of tic tac toe class

---
+++ src/tic-tac-toe.js
@@ -0,0 +1,67 @@
+
+
+const boardSpaces  = {
+    EMPTY: 0,
+    X: 1,
+    O: 2
+}
+
+class TicTacToe {
+    constructor(boardSize) {
+        this._boardSize = boardSize;
+
+        // initialize the board matrix
+        this._board = new Array(this._boardSize);
+        for (let i = 0; i < this._board.length; i++) {
+            this._board[i] = new Array.from(Array(this._boardSize), () => boardSpaces.EMPTY);
+        }
+
+        this._turn = boardSpaces.X;
+
+        this.getSpace = this.getSpace.bind(this);
+        this.move = this.move.bind(this);
+        this.undo = this.undo.bind(this);
+        this.copy = this.copy.bind(this);
+    }
+
+    get boardSize() {
+        return this._boardSize;
+    }
+
+    get turn() {
+        return this._turn
+    }
+
+    getSpace(x, y) {
+        return this._board[x][y];
+    }
+
+    move(x, y) {
+        if (x >= this._boardSize) {
+            throw "Provided x value exceeds size of Tic-Tac-Toe board!";
+        }
+        else if (y >= this._boardSize) {
+            throw "Provided y value exceeds size of Tic-Tac-Toe board!";
+        }
+        else if (this._board[x][y] !== boardSpaces.EMPTY) {
+            throw "Board location already played!";
+        }
+        else {
+            // mark the board at specified location
+            this._board[x][y] = this._turn;
+            // switch turns
+            this._turn = (this._turn === boardSpaces.X) ? boardSpaces.O : boardSpaces.X;
+        }
+    }
+
+
+    // TODO: Implement ability to undo arbitrary number of moves. Useful for Mini-Max.
+    undo() {
+
+    }
+
+    // TODO: Implement a copy constructor-like method.
+    copy() {
+
+    }
+}
