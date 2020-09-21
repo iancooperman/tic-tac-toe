774fb23620ef7febea48d4c22a2f3fc4e378e022
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-09-20 17:19:19 -0700

    Implemented and tested the undoing of moves

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -13,12 +13,15 @@ export class TicTacToe {
             this._board[i] = Array.from(Array(boardSize), () => boardSpaces.EMPTY);
         }
 
+
         this._turn = boardSpaces.X;
+        this._moves = new Array();
 
         this.getSpace = this.space.bind(this);
         this.move = this.move.bind(this);
         this.undo = this.undo.bind(this);
         this.copy = this.copy.bind(this);
+        this._switchTurns = this._switchTurns.bind(this);
     }
 
     get boardSize() {
@@ -43,22 +46,59 @@ export class TicTacToe {
         else if (this._board[x][y] !== boardSpaces.EMPTY) {
             throw "Board location already played!";
         }
-        else {
+        else { // move is acceptable
             // mark the board at specified location
             this._board[x][y] = this._turn;
             // switch turns
-            this._turn = (this._turn === boardSpaces.X) ? boardSpaces.O : boardSpaces.X;
+            this._switchTurns();
+            this._moves.push(new Move(x, y));
         }
+    }
+
+    _switchTurns() {
+        this._turn = (this._turn === boardSpaces.X) ? boardSpaces.O : boardSpaces.X;
     }
 
 
     // TODO: Implement ability to undo arbitrary number of moves. Useful for Mini-Max.
     undo() {
+        // If there are no moves to undo, just say so!
+        if (this._moves.length === 0) {
+            throw "No moves to undo!"
+        }
+
+        let previousMove = this._moves.pop();
+        let x = previousMove.x;
+        let y = previousMove.y;
+        this._board[x][y] = boardSpaces.EMPTY;
+
+        this._switchTurns();
+    }
+
+    // TODO: Implement a copy-constructor-like method.
+    copy() {
 
     }
 
-    // TODO: Implement a copy constructor-like method.
-    copy() {
+    // TODO: Implement win checking.
+    isWin() {
 
+    }
+}
+
+// All this class does is store coordinates for moves taken.
+// There's no validation. That happens in `TicTacToe`
+class Move {
+    constructor(x, y) {
+        this._x = x;
+        this._y = y;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    get y() {
+        return this._y;
     }
 }
--- src/tic-tac-toe.test.js
+++ src/tic-tac-toe.test.js
@@ -37,3 +37,23 @@ test('move location cannot exceed board boundaries', () => {
    expect(() => {board.move(0, 3)}).toThrow();
    expect(() => {board.move(3, 0)}).toThrow();
 });
+
+test('moves can be undone', () => {
+   let board = new TicTacToe(3);
+   board.move(0, 0);
+   board.move(1, 1);
+   board.move(2, 2);
+   board.undo();
+   board.undo();
+   board.move(0, 2);
+
+   expect(board.getSpace(2, 2)).toEqual(boardSpaces.EMPTY);
+   expect(board.getSpace(1, 1)).toEqual(boardSpaces.EMPTY);
+   expect(board.getSpace(0, 0)).toEqual(boardSpaces.X);
+   expect(board.getSpace(0, 2)).toEqual(boardSpaces.O);
+});
+
+test('moves can\'t be undone if there are no moves to undo', () => {
+   let board = new TicTacToe(3);
+   expect(() => {board.undo()}).toThrow();
+});
